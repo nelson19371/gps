9b4701ea1bda0b43656257b1c9361d503caca2ee
nelson19371 <sandoval19371@gmail.com>
2022-09-05 15:47:27 -0300

    Primer comit

---
+++ lib/widgets/scan_button.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+
+class ScanButton extends StatelessWidget {
+  
+
+  @override
+  Widget build(BuildContext context) {
+    return FloatingActionButton(
+      child: Icon(Icons.filter_center_focus),
+      onPressed: null,
+    );
+  }
+}
